cbe14ac70de32589394d6fb736442e50ebdf93ec
ElHuaco <alexhuac@gmail.com>
2021-05-17 11:59:56 +0200

    Queue done

---
+++ Queue/queue.hpp
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   queue.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandroleon <aleon-ca@student.42.fr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/17 11:56:25 by alejandro         #+#    #+#             */
+/*   Updated: 2021/05/17 11:59:14 by alejandro        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef QUEUE_HPP
+# define QUEUE_HPP
+
+# include "../List/list.hpp"
+
+namespace ft
+{
+	typename <class T, class Container = ft::list<T> > class queue
+	{
+		public:
+			typename T			value_type;
+			typename Container	container_type;
+
+		private:
+			container_type		_c;
+			queue(const queue &other);
+
+		public:
+			explicit queue(const container_type &cntr = container_type()) :
+				_c(cntr)
+			{
+			}
+			~queue(void)
+			{
+			}
+			queue		&operator=(const queue &rhs)
+			{
+				if (this == &rhs)
+					return (*this);
+				this->_c = rhs._c;
+				return (*this);
+			}
+			bool		empty(void) const
+			{
+				return (this->_c.empty());
+			}
+			size_type	size(void) const
+			{
+				return (this->_c.size());
+			}
+			value_type	&back(void)
+			{
+				return (this->_c.back());
+			}
+			const value_type	&back(void) const
+			{
+				return (this->_c.back());
+			}
+			value_type	&front(void)
+			{
+				return (this->_c.front());
+			}
+			const value_type	&front(void) const
+			{
+				return (this->_c.front());
+			}
+			void		push(const value_type &val)
+			{
+				this->_c.push_back(val);
+			}
+			void		pop(void)
+			{
+				this->_c.pop_front();
+			}
+	};
+	template <class T, class Container>
+	bool operator==(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
+	{
+		return (lhs._c == rhs._c);
+	}
+	template <class T, class Container>
+	bool operator!=(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
+	{
+		return (lhs._c != rhs._c);
+	}
+	template <class T, class Container>
+	bool operator<(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
+	{
+		return (lhs._c < rhs._c);
+	}
+	template <class T, class Container>
+	bool operator<=(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
+	{
+		return (lhs._c <= rhs._c);
+	}
+	template <class T, class Container>
+	bool operator>(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
+	{
+		return (lhs._c > rhs._c);
+	}
+	template <class T, class Container>
+	bool operator>=(const queue<T, Container> &lhs, const queue<T, Container> &rhs)
+	{
+		return (lhs._c >= rhs._c);
+	}
+};
+#endif
