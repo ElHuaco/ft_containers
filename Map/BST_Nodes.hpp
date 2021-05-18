da826f6194b309e1679c23d633edc8bdf2183141
ElHuaco <alexhuac@gmail.com>
2021-05-18 10:08:38 +0200

    Starting map

---
+++ Map/BST_Nodes.hpp
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BST_Nodes.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejandroleon <aleon-ca@student.42.fr      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/18 09:46:29 by alejandro         #+#    #+#             */
+/*   Updated: 2021/05/18 10:03:26 by alejandro        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BST_NODES_HPP
+# define BST_NODES_HPP
+
+namespace ft
+{
+	template <class T> class BSTNode
+	{
+		public:
+			typedef T					value_type;
+			typedef BSTNode<value_type>	m_node;
+			
+		private:
+			value_type		_data;
+			m_node			*_left;
+			m_node			*_right;
+			m_node			*_parent;
+
+		public:
+			/* COPLIEN */
+			explicit Node(const value_type &data = value_type()) : _data(data),
+				_left(nullptr), _right(nullptr), _parent(nullptr)
+			{
+			}
+			Node(const Node &other)
+			{
+				*this = other;
+			}
+			~Node(void)
+			{
+			}
+			Node	&operator=(const Node &rhs)
+			{
+				if (this == &rhs)
+					return (*this);
+				this->_data = rhs._data;
+				this->_left = rhs._left;
+				this->_right = rhs._right;
+				this->_parent = rhs._parent;
+				return (*this);
+			}
+			/* SET(GET)TERS */
+			m_node		*&left(void)
+			{
+				return (this->_left);
+			}
+			m_node		*&right(void)
+			{
+				return (this->_right);
+			}
+			m_node		*&parent(void)
+			{
+				return (this->_parent);
+			}
+			m_node * const &left(void) const
+			{
+				return (this->_left);
+			}
+			m_node * const &right(void) const
+			{
+				return (this->_right);
+			}
+			m_node * const &parent(void) const
+			{
+				return (this->_parent);
+			}
+			value_type	&getData(void)
+			{
+				return (this->_data);
+			}
+			const value_type &getData(void) const
+			{
+				return (this->_data);
+			}	
+	};
+};
+#endif
--- Map/map.hpp
+++ Map/map.hpp
@@ -6,16 +6,19 @@
 /*   By: alejandroleon <aleon-ca@student.42.fr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 08:50:51 by alejandro         #+#    #+#             */
-/*   Updated: 2021/05/18 09:16:41 by alejandro        ###   ########.fr       */
+/*   Updated: 2021/05/18 10:07:50 by alejandro        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_HPP
 # define MAP_HPP
 
+# include "../shared_tools/disable_if.hpp"
 # include "../shared_tools/predicates.hpp"
 # include "BidirectionalMapIterator.hpp"
+# include "BST_Nodes.hpp"
 # include <utility>
+# include <limits>
 # include <functional>
 
 namespace ft
@@ -24,6 +27,9 @@ namespace ft
 		class Alloc = std::allocator<std::pair<const Key, T> > class map
 	{
 		public:
+			/****************************/
+			/*       MEMBER TYPES       */
+			/****************************/
 			typedef Key										key_type;
 			typedef T										mapped_type;
 			typedef pair<const key_type, mapped_type>		value_type;
@@ -33,6 +39,7 @@ namespace ft
 			typedef allocator_type::const_reference			const_reference
 			typedef allocator_type::pointer					pointer
 			typedef allocator_type::const_pointer			const_pointer
+			typedef BSTNode<value_type>						node;
 			typedef BidirectionalMapIterator<value_type>	iterator;
 			typedef BidirectionalMapIterator<const value_type>	const_iterator;
 			typedef ReverseMapIterator<iterator>			reverse_iterator;
@@ -58,6 +65,42 @@ namespace ft
 						return comp(x.first, y.first);
 					}
 			};
+
+		private:
+			/****************************/
+			/*        ATTRIBUTES        */
+			/****************************/
+			node			*_root;
+			size_type		_size;
+
+		public:
+			/****************************/
+			/* COPLIEN MEMBER FUNCTIONS */
+			/****************************/
+			explicit map(const key_compare &comp = key_compare(),
+				const allocator_type &alloc = allocator_type()) :
+				_root(nullptr), _size(0)
+			{
+			}
+			template <class InputIterator>
+			map (InputIterator first, InputIterator last,
+				const key_compare &comp = key_compare(),
+				const allocator_type &alloc = allocator_type(),
+				typename ft::disable_if
+					<ft::is_integral<InputIterator> >::type* dummy = 0) :
+				_root(nullptr), _size(0)
+			{
+			}
+			map(const map &other)
+			{
+				*this = other;
+			}
+			~map(void)
+			{
+			}
+			map	&operator=(const map &rhs)
+			{
+			}
 	};
 };
 #endif
